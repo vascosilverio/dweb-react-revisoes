004cdcdcd064f6223e8e8d2600425a1511bfda62
João Silva <jotal96@gmail.com>
2024-02-27 11:42:26 +0000

    vai buscar tarefas à API

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Todo() {
     const [tarefa, setTarefa] = useState('');
     const [dataConc, setDataConc] = useState(new Date());
     const [listaTarefas, setLista] = useState([]);
 
-    const addTarefa = () => {
-        alert("Ola");
+    useEffect(() => {
+        getTarefas();
+    }, []);
+
+
+    // metodo nao bloqueante
+    const getTarefas = async () => {
+        fetch("https://spring-server.azurewebsites.net/todo/getTarefas")
+            .then(res => {
+                return res.json();
+            })
+            .then((res) => {
+                setLista(res.rows);
+            });
+    }
+
+    // metodo bloqueante
+    const getTarefasSincrono = async () => {
+        let res = await fetch("https://spring-server.azurewebsites.net/todo/getTarefas");
+        res = res.json();
+    }
+
+    const addTarefaApi = () => {
+
     }
 
     return <>
@@ -27,9 +49,18 @@ function Todo() {
         </div>
 
         <div className="row mt-5">
-            <div className="col-md-4 col-sm-9"><input className='form-control' type='button' onClick={() => {addTarefa() }} value={"Adicionar Tarefa"} /></div>
-            
+            <div className="col-md-4 col-sm-9"><input className='form-control' type='button' onClick={() => { getTarefasSincrono() }} value={"Adicionar Tarefa"} /></div>
+
         </div>
+
+        <ul className="mt-5">
+            {
+                listaTarefas.map(elem => {
+                    return <li className="form-control">{elem.descricao}</li>
+                })
+            }
+
+        </ul>
     </>;
 }
 

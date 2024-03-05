bfd3894623ce61ef385b40f98662145c28ef3af0
João Silva <jotal96@gmail.com>
2024-03-05 18:36:14 +0000

    delete with modal and added function that runs on a set interval

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,21 +1,37 @@
 import { useEffect, useState } from "react";
 import { createTarefaApi, deleteTarefasApi, getTarefasApi } from "../service/api";
 
+// imports for the modal
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 function Todo() {
     const [desc, setDesc] = useState('');
     const [dataConc, setDataConc] = useState(new Date().toISOString().split("T")[0]);
     const [listaTarefas, setLista] = useState([]);
 
-    const handleDeleteTarefa = (id) => {
-        deleteTarefasApi(id)
-        .then((res) => {
-            return res.json();
-        })
-        .then((res)=>{
-           handleGetListaTarefas();
-        });
+    // variable to be used when trying to delete a task
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [idTaskDel, setIdTaskDel] = useState(0);
 
+    const handleCloseDeleteModal = (isToDelete) =>{
+        if(isToDelete){
+            deleteTarefasApi(idTaskDel)
+            .then((res) => {
+                return res.json();
+            })
+            .then((res)=>{
+            });
+        }
         
+        setShowDeleteModal(false);
+    } ;
+    const handleShowDeleteModal = () => setShowDeleteModal(true);
+
+    const handleDeleteTarefa = (id) => {
+        setIdTaskDel(id)
+
+        handleShowDeleteModal();
     }
 
     // atualiza a lista de tarefas da API
@@ -45,15 +61,16 @@ function Todo() {
         createTarefaApi(tarefa)
             .then(res => res.json())
             .then(res => {
-                    setDesc('');
-                    alert('Tarefa criada com sucesso');
-                    handleGetListaTarefas();
+                setDesc('');
+                alert('Tarefa criada com sucesso');
             });
     }
 
     // corre a primeira vez que o componente monta
     useEffect(() => {
-        handleGetListaTarefas();
+        setInterval(()=>{
+            handleGetListaTarefas();
+        }, 1000);
     }, []);
 
 
@@ -88,18 +105,36 @@ function Todo() {
                         <div className="row">
                             <div className="col-md-5 col-sm-12">
                                 <h4>{tarefa.descricao}</h4>
+                                <p>{tarefa.id}</p>
                             </div>
                             <div className="col-md-4 col-sm-8">
                                 <p>Concluir até {tarefa.dataConclusao}</p>
                             </div>
                             <div className="col-md-3 col-sm-4">
-                                <button onClick={()=> {handleDeleteTarefa(tarefa.id)}} type="button" class="btn btn-danger float-end">Danger</button>
+                                <button onClick={() => { handleDeleteTarefa(tarefa.id) }} type="button" class="btn btn-danger float-end">Delete</button>
                             </div>
                         </div>
                     </li>
                 })
             }
         </ul>
+
+        <Modal show={showDeleteModal} onHide={()=> {
+            handleCloseDeleteModal(false);
+        }}>
+            <Modal.Header closeButton>
+                <Modal.Title>Do you really wanna delete the task with id: {idTaskDel}</Modal.Title>
+            </Modal.Header>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={()=> handleCloseDeleteModal(false)}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={() => handleCloseDeleteModal(true)}>
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
     </>;
 }
 
